7683ace0c29b65aaf0559c18c40c47d4975e7ec3
Persing <NicholasPersing@gmail.com>
2015-11-15 17:35:41 -0500

    minnor changes

--- SimpleElevator.java
+++ SimpleElevator.java
@@ -8,6 +8,10 @@ import java.util.Queue;
 public class SimpleElevator extends Elevator{
     
     
+    Queue<ElevatorProj.PassengerRequest>[][] floorsQ;
+    Queue<ElevatorProj.PassengerRequest>[] carrage;
+    boolean direction;
+    
     public SimpleElevator(int capacity, int timeMoveOneFloor, 
 			int floors, int doorDelta, boolean verbose){
         super(capacity, timeMoveOneFloor, floors, doorDelta, verbose);
@@ -15,7 +19,12 @@ public class SimpleElevator extends Elevator{
     
     public void initialize(Queue<ElevatorProj.PassengerRequest> requests) {
 		Elevator.servingQueue = requests;
-                Queue<ElevatorProj.PassengerRequest>[][] floorsQ = new Queue[Elevator.floors][2];
+                //0 is down 1 is up!
+                floorsQ = new Queue[Elevator.floors][2];
+                carrage = new Queue[Elevator.floors];
+                
+                //false is down, true is up
+                direction = true;
                 
                 //initialize all of the queues
                 for(int i=0;i < floorsQ.length; i++){
@@ -30,10 +39,15 @@ public class SimpleElevator extends Elevator{
 		ArrayList<ElevatorProj.PassengerReleased> released =
 					new ArrayList<>();
                 
-                while(Elevator.servingQueue.peek().getTimePressedButton().getTime
-                        <= Elevator.currentTime.getTime){
+                while(!Elevator.servingQueue.isEmpty() 
+                        && Elevator.servingQueue.peek().getTimePressedButton().getTime
+                        <= Elevator.currentTime.getTime)
+                    addToFloors(Elevator.servingQueue.pop());
+                
+                if(isCarrageEmpty()){
                     
                 }
+                
                 
     }
     
@@ -41,10 +55,16 @@ public class SimpleElevator extends Elevator{
         return !Elevator.servingQueue.isEmpty();
     }
     
+    
     private void addToFloors(PassengerRequest passenger){
-        if(passenger.getFloorFrom()<passenger.getFloorTo()){
-            
-        }
+        if(passenger.getFloorFrom()<passenger.getFloorTo())
+            floorsQ[passenger.getFloorFrom()][1]=passenger;
+        else
+             floorsQ[passenger.getFloorFrom()][0]=passenger;
+    }
+    
+    private boolean isCarrageEmpty(){
+        return Elevator.weight==0;
     }
     
     
